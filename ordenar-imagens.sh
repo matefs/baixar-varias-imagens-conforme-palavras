d14ebaae4f9ea76149049cad2abfc986808faa6d
Mateus Schverz <mate_fs@hotmail.com>
2023-10-01 14:11:03 -0300

    chore: ultima data de alteração

--- ordenar-imagens.sh
+++ ordenar-imagens.sh
@@ -6,8 +6,8 @@ diretorio="."
 # Nome do arquivo de texto de saída
 arquivo_saida="imagens_paths.txt"
 
-# Lista apenas os caminhos das imagens no diretório e subpastas por ordem de criação e salva em um arquivo de texto
-find "$diretorio" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -exec echo {} \; | sort > "$arquivo_saida"
+# Lista apenas os caminhos das imagens no diretório e subpastas por ordem de modificação e salva em um arquivo de texto
+find "$diretorio" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -exec ls -lt {} + | awk '{print $9}' > "$arquivo_saida"
 
 echo "Caminhos das imagens foram salvos em $arquivo_saida."
 
